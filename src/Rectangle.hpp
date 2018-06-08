d1bc354ef3e2be60ec2686ed972f1134a2fced1b
Vincent <chenjuzen@gmail.com>
2018-06-08 14:57:26 +0800

    add point, rectangle, vertex

---
+++ src/Rectangle.hpp
@@ -0,0 +1,10 @@
+#ifndef VLSI_FIANL_PROJECT_RECTANGLE_HPP_
+#define VLSI_FIANL_PROJECT_RECTANGLE_HPP_
+#include "Point.hpp"
+class Rectangle
+{
+public:
+	Point lower_left, upper_right;
+};
+
+#endif
---
+++ src/Vertex.hpp
@@ -0,0 +1,14 @@
+#ifndef VLSI_FIANL_PROJECT_VERTEX_HPP_
+#define VLSI_FIANL_PROJECT_VERTEX_HPP_
+
+class Vertex
+{
+public:
+    int x, y;
+    int layer;
+    int id;
+    int connect_id;
+
+};
+
+#endif
