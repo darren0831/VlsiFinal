e7c51f765fc930a3b36f3e22942e3e031f3c8079
ax2082197 <ax2082197@gmail.com>
2018-06-08 15:57:47 +0800

    Track

---
+++ src/Track.hpp
@@ -0,0 +1,17 @@
+#ifndef VLSI_FIANL_PROJECT_POINT_HPP_
+#define VLSI_FIANL_PROJECT_POINT_HPP_
+class Track
+{
+public:
+    Track(){};
+    void initial(double _lx,double _ly,double _rx,double _ry,double _w,std::string _name,int _layer){
+        rect(_lx,_ly-_w/2,_rx,_ry+_w/2);
+        name = _name;
+        layer = _layer;
+    }
+	Rectangle rect;
+	std::string name;
+	int layer;
+};
+
+#endif
