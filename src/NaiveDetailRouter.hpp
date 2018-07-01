a6cfe7d5da921944c04c70e4c0b7f77a6879d6fe
vincent <chenjuzen@gmail.com>
2018-07-01 11:07:14 +0800

    use A*search to route

--- src/NaiveDetailRouter.hpp
+++ src/NaiveDetailRouter.hpp
@@ -1,6 +1,8 @@
 #ifndef VLSI_FINAL_PROJECT_NAIVE_DETAIL_ROUTER_PATH_HPP_
 #define VLSI_FINAL_PROJECT_NAIVE_DETAIL_ROUTER_PATH_HPP_
 class NaiveDetailRouter {
+private:
+	int VIA_COST=0;
 public:
 	NaiveDetailRouter(std::vector<Vertex>& vertices,
 		std::vector<Bus>& buses,
@@ -31,6 +33,7 @@ public:
 				else{
 					if(flag)
 						routeOtherBit(j,bit,n);
+
 				}
 			}
 		}
@@ -42,7 +45,7 @@ public:
 			std::vector<int> endVertexId;
 			logger.show("    - Pin %d:\n",i);
 			int startVertexId = bit[i];
-			candidateVertexId = std::queue<DetailNode>();
+			candidateVertexId = std::priority_queue<DetailNode>();
 			if(i==1)
 				endVertexId.emplace_back(bit[0]);
 			else{
@@ -56,13 +59,16 @@ public:
 			// 	logger.show("      endVertexId: %d\n",end);
 			// }
 			detailPath.clear();
-			DetailNode start(startVertexId,-1);
+			double x = vertices[startVertexId].track.rect.midPoint().x;
+			double y = vertices[startVertexId].track.rect.midPoint().y;
+			DetailNode start(x,y,0,0,startVertexId,-1);
 			candidateVertexId.push(start);
 			bool flag = false;
 			std::vector<int> prev = std::vector<int>(vertices.size(),-1);
+			std::vector<int> preveid = std::vector<int>(vertices.size(),-1);
 			int currentVertexId=-1;
 			while(!candidateVertexId.empty()){
-				DetailNode currentNode = candidateVertexId.front();
+				DetailNode currentNode = candidateVertexId.top();
 				currentVertexId = currentNode.vertexId;
 				candidateVertexId.pop();
 				for(int end: endVertexId){
@@ -75,15 +81,20 @@ public:
 				for(int eid : routingGraph[currentVertexId]){
 					int tgtId = routingEdges[eid].getTarget(currentVertexId);
 					if(prev[tgtId] == -1 && prev[currentVertexId]!=tgtId && prev[tgtId]!=startVertexId && isVertexUsed.find(tgtId)==isVertexUsed.end()){
-						candidateVertexId.push(DetailNode(tgtId,-1));
+						double nextX = vertices[tgtId].track.rect.midPoint().x;
+                        double nextY = vertices[tgtId].track.rect.midPoint().y;
+                        std::pair<double,double> distanceCost = calDistanceCost( eid, tgtId, currentVertexId, currentNode, endVertexId);
+
+						candidateVertexId.push(DetailNode(nextX, nextY, distanceCost.first, distanceCost.second, tgtId, -1));
 						prev[tgtId] = currentVertexId;
+						preveid[tgtId] = eid;
 					}
 				}
 			}
 			if(flag){
 				logger.show("      Do Detail Back Trace\n");
 				while(currentVertexId!=-1){
-					// logger.show("%d\n",currentVertexId);
+					logger.show("%d, layer: %d\n",currentVertexId, vertices[currentVertexId].track.layer);
 					detailPath.emplace_back(currentVertexId);
 					curNet.insert(currentVertexId);
 					isVertexUsed.insert(currentVertexId);
@@ -117,7 +128,7 @@ public:
 			logger.show("    - Pin %d:\n",i);
 			int startVertexId = bit[i];
 			int curFollowId = 0;
-			candidateVertexId = std::queue<DetailNode>();
+			candidateVertexId = std::priority_queue<DetailNode>();
 			if(i==1)
 				endVertexId.emplace_back(bit[0]);
 			else{
@@ -131,13 +142,16 @@ public:
 			// 	logger.show("      endVertexId: %d\n",end);
 			// }
 			detailPath.clear();
-			DetailNode start(startVertexId,0);
+			double x = vertices[startVertexId].track.rect.midPoint().x;
+			double y = vertices[startVertexId].track.rect.midPoint().y;
+			DetailNode start(x,y,0,0,startVertexId,-1);
 			candidateVertexId.push(start);
 			bool flag = false;
 			std::vector<int> prev = std::vector<int>(vertices.size(),-1);
+			std::vector<int> preveid = std::vector<int>(vertices.size(),-1);
 			int currentVertexId=-1;
 			while(!candidateVertexId.empty()){
-				DetailNode currentNode = candidateVertexId.front();
+				DetailNode currentNode = candidateVertexId.top();
 				currentVertexId = currentNode.vertexId;
 				curFollowId = currentNode.followId;
 				candidateVertexId.pop();
@@ -152,8 +166,13 @@ public:
 					int tgtId = routingEdges[eid].getTarget(currentVertexId);
 					if(prev[tgtId] == -1 && prev[currentVertexId]!=tgtId && prev[tgtId]!=startVertexId && isVertexUsed.find(tgtId)==isVertexUsed.end()){
 						if((direction[i-1][curFollowId]==routingEdges[eid].getDirection(currentVertexId,tgtId)||routingEdges[eid].getDirection(currentVertexId,tgtId)==' ') && curFollowId < (int)direction[i-1].size()){
-							candidateVertexId.push(DetailNode(tgtId,curFollowId+1));
+							double nextX = vertices[tgtId].track.rect.midPoint().x;
+	                        double nextY = vertices[tgtId].track.rect.midPoint().y;
+	                        std::pair<double,double> distanceCost = calDistanceCost( eid, tgtId, currentVertexId, currentNode, endVertexId);
+
+							candidateVertexId.push(DetailNode(nextX, nextY, distanceCost.first, distanceCost.second, tgtId, curFollowId+1));
 							prev[tgtId] = currentVertexId;
+							preveid[tgtId] = eid;
 						}
 					}
 				}
@@ -162,7 +181,7 @@ public:
 			if(flag){
 				logger.show("      Do Detail Back Trace\n");
 				while(currentVertexId!=-1){
-					// logger.show("%d\n",currentVertexId);
+					logger.show("%d, layer: %d\n",currentVertexId, vertices[currentVertexId].track.layer);
 					detailPath.emplace_back(currentVertexId);
 					curNet.insert(currentVertexId);
 					isVertexUsed.insert(currentVertexId);
@@ -179,11 +198,62 @@ public:
 private:
 	class DetailNode{
 	public:
-		DetailNode(int vertexId,int followId):vertexId(vertexId),followId(followId){}
-		int vertexId;
+		int curX, curY;
+		double curDistance;
+        double assumeDistance;
+        int vertexId;
 		int followId;
+	public:
+		DetailNode(int curX, int curY, double curDistance, double assumeDistance, int vertexId,int followId):
+		curX(curX), curY(curY), curDistance(curDistance), assumeDistance(assumeDistance), vertexId(vertexId),followId(followId){}
+        bool operator<(const DetailNode &that) const{
+            double cost = curDistance+assumeDistance;
+            double thatCost = that.curDistance + that.assumeDistance;
+            if (fabs(cost - thatCost) > 1e-6) {
+                return cost < thatCost;
+            }else
+                return false;
+        }
+    
 	};
-
+public:
+	double assumeDistance(const double curX,const double curY, const std::vector<int>& endVertexId){
+        double maxX = -1, maxY = -1, minX = -1, minY = -1;
+        for(unsigned int i=0;i<endVertexId.size();i++){
+            double curX = vertices[endVertexId[i]].track.rect.midPoint().x;
+            double curY = vertices[endVertexId[i]].track.rect.midPoint().y;
+            if(curX>maxX || maxX == -1 ) maxX = curX;
+            if(curY>maxY || maxY == -1 ) maxY = curY;
+            if(curX<minX || minX == -1 ) minX = curX;
+            if(curY<minY || minY == -1 ) minY = curY;
+        }
+        maxX  = std::max(maxX, curX);
+        maxY  = std::max(maxY, curY);
+        minX  = std::min(minX, curX);
+        minY  = std::min(minY, curY);
+        return (maxX-minX)+(maxY-minY);
+    }
+    std::pair<double, double> calDistanceCost(const int& eid, const int& tgtId,const int& currentVertexId, const DetailNode& curNode, const std::vector<int>& endVertexId){
+        char edgeDir = routingEdges[eid].getDirection(currentVertexId,tgtId);
+        Vertex& nextVertex = vertices[tgtId];
+        double nextX = nextVertex.track.rect.midPoint().x;
+        double nextY = nextVertex.track.rect.midPoint().y;
+        double nextDistance, nextAssume;
+        if(edgeDir== 'L' || edgeDir== 'R'){
+            nextDistance = curNode.curDistance + fabs(curNode.curX - nextX);
+            nextAssume = assumeDistance(nextX,nextY,endVertexId);
+        }else if(edgeDir== 'F' || edgeDir== 'B'){
+            nextDistance = curNode.curDistance + fabs(curNode.curY - nextY);
+            nextAssume = assumeDistance(nextX,nextY,endVertexId);
+        }else if(edgeDir== 'U' || edgeDir== 'D'){
+            nextDistance = curNode.curDistance;
+            nextAssume = curNode.assumeDistance + VIA_COST;
+        }else{
+            nextDistance = 0;
+            nextAssume = 0;
+        }
+        return std::make_pair(nextDistance,nextAssume);
+    }
 public:
 	std::vector<Vertex>& vertices;
 	std::vector<Bus>& buses;
@@ -194,7 +264,7 @@ public:
 public:
 	Logger& logger;
 	std::vector<int> detailPath;
-	std::queue<DetailNode> candidateVertexId;
+	std::priority_queue<DetailNode> candidateVertexId;
 	std::unordered_set<int> isVertexUsed;
 	std::vector<std::vector<char>> direction;
 	std::unordered_set<int> curNet; 
