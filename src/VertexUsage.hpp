c230e2c6897544f44235335e888d2a552956b993
LittleBird <ilms49898723@gmail.com>
2018-07-01 15:45:10 +0900

    vertex usage segment

--- src/VertexUsage.hpp
+++ src/VertexUsage.hpp
@@ -2,7 +2,9 @@
 #define VLSI_FINAL_PROJECT_VERTEX_USAGE_HPP_
 
 #include <algorithm>
+#include <utility>
 #include <vector>
+#include "Vertex.hpp"
 
 class VertexUsage {
 public:
@@ -11,7 +13,8 @@ public:
 
     VertexUsage(int width) :
         width(width) {
-        record = std::vector<bool>(width);
+        record = std::vector<bool>(width + 8);
+        initialize();
     }
 
     void initialize() {
@@ -20,19 +23,55 @@ public:
         }
     }
 
-    bool query(const Vertex& src, const Vertex& current, const Vertex& tgt) {
-        
+    bool query(const Vertex& source, const Vertex& current, const Vertex& target) {
+        auto segment = getSegment(source, current, target);
+        checkBound(segment.first, segment.second);
+        for (int i = segment.first; i <= segment.second; ++i) {
+            if (record[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    void set(const Vertex& src, const Vertex& current, const Vertex& tgt) {
-        
+    void set(const Vertex& source, const Vertex& current, const Vertex& target) {
+        auto segment = getSegment(source, current, target);
+        checkBound(segment.first, segment.second);
+        for (int i = segment.first; i <= segment.second; ++i) {
+            record[i] = true;
+        }
     }
 
-    void unset(const Vertex& src, const Vertex& current, const Vertex& tgt) {
-
+    void unset(const Vertex& source, const Vertex& current, const Vertex& target) {
+        auto segment = getSegment(source, current, target);
+        checkBound(segment.first, segment.second);
+        for (int i = segment.first; i <= segment.second; ++i) {
+            record[i] = false;
+        }
     }
 
+    std::pair<int, int> getSegment(const Vertex& source, const Vertex& current, const Vertex& target) const {
+        Rectangle r1 = current.track.overlap(source.track);
+        Rectangle r2 = current.track.overlap(target.track);
+        if (current.track.isHorizontal()) {
+            int start = (int) (floor(std::min(r1.ll.x, r2.ll.x)));
+            int end = (int) (ceil(std::max(r1.ur.x, r2.ur.x)));
+            return std::make_pair(start, end);
+        } else {
+            int start = (int) (floor(std::min(r1.ll.y, r2.ll.y)));
+            int end = (int) (ceil(std::max(r1.ur.y, r2.ur.y)));
+            return std::make_pair(start, end);
+        }
+    }
 
+    void checkBound(int start, int end) {
+#ifdef VLSI_FINAL_PROJECT_DEBUG_FLAG
+        if (start < 0 || start > width || end < 0 || end > width) {
+            fprintf(stderr, "[Fatal Error] Segment [%d, %d] can not be covered by [%d, %d]\n",
+                start, end, 0, width);
+        }
+#endif
+    }
 
 private:
     int width;
